2e46b5c79c6d188ca412832e89f9409aca046052
Huy Tran <huy.td183768@sis.hust.edu.vn>
2021-05-26 20:33:49 +0700

    Update: Index

--- application/views/index/guest.php
+++ application/views/index/guest.php
@@ -1,20 +1,20 @@
-<?php require_once(LOG_IN);?>
-<h1>GUEST PAGE</h1>
-<h3>This list of artists</h3>
+<h1 class="front-text">Welcome to HustMP3</h1>
+<h3 class="sub-header">Choose your favorite artist and enjoy the music</h3>
 
-<?php 
+<?php
 $number = 0;
 
 ?>
-<?php foreach ($artist as $artistitem):?>
-	<?php $path = BASE_PATH."/artist/viewdetail/".$artistitem['Artist']['id']."/".strtolower(str_replace(" ","-",$artistitem['Artist']['name'])); ?>
-	<a class="big" href="<?php echo  $path?>">
-	<span class="artist">
-	<?php echo ++$number?>
-	<?php echo $artistitem['Artist']['name']?>
-	</span>
-	</a><br/>
-<?php endforeach?>
-<button><a href=<?php echo SONG_VIEWALL;?>>All songs</a></button>
-<br/>
-<button><a href=<?php echo ARTIST_VIEWALL?>>All artists</a></button>
+<div class="artist-list">
+	<?php foreach ($artist as $artistitem) : ?>
+		<?php $path = BASE_PATH . "/artist/viewdetail/" . $artistitem['Artist']['id'] . "/" . strtolower(str_replace(" ", "-", $artistitem['Artist']['name'])); ?>
+		<a class="big" href="<?php echo  $path ?>">
+			<div class="artist" style="background-image: url(<?php echo BASE_PATH."/public/assets/img/".$artistitem['Artist']['avatar'] ?>)">
+				<!--
+					<?php echo ++$number ?> 
+				<?php echo $artistitem['Artist']['name'] ?>
+				-->
+			</div>
+		</a>
+	<?php endforeach ?>
+</div>
